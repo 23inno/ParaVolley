f15d458d867b14867e426d7d17d9f456b7bad32c
KKhunou <kamohelokhunou23@gmail.com>
2026-09-16 13:11:32 +0200

    Add new color definitions for theme

--- mobile/app/src/main/java/com/paravolley/mobile/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/paravolley/mobile/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.paravolley.mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green80 = Color(0xFF86E0B8)
+val GreenGrey80 = Color(0xFFB5D1C5)
+val Gold80 = Color(0xFFFFE082)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green40 = Color(0xFF0B6E4F)
+val GreenGrey40 = Color(0xFF4A6357)
+val Gold40 = Color(0xFFD4A017)
