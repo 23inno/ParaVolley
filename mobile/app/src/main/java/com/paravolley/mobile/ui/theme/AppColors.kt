7bb8275a5251e222327450287935084361b92b07
KKhunou <kamohelokhunou23@gmail.com>
2026-09-16 13:09:45 +0200

    Revise AppColors with official brand colors
    Updated brand colors to reflect official ParaVolley Mpumalanga colors and added new color definitions.

--- mobile/app/src/main/java/com/paravolley/mobile/ui/theme/AppColors.kt
+++ mobile/app/src/main/java/com/paravolley/mobile/ui/theme/AppColors.kt
@@ -3,12 +3,26 @@ package com.paravolley.mobile.ui.theme
 import androidx.compose.ui.graphics.Color
 
 object AppColors {
-    val DarkGreen = Color(0xFF155D3A)
-    val Green = Color(0xFF1F7048)
-    val LightGreen = Color(0xFFE8F4ED)
-    val Yellow = Color(0xFFFFC226)
-    val DarkText = Color(0xFF1D2A23)
-    val GreyText = Color(0xFF66736C)
-    val LightBackground = Color(0xFFF5F7F6)
+    // Official ParaVolley Mpumalanga Brand Colors
+    val DarkGreen = Color(0xFF08523B)
+    val Green = Color(0xFF0B6E4F)
+    val LightGreen = Color(0xFFEAF5F0)
+    val SoftGreen = Color(0xFFD1FAE5)
+
+    val Yellow = Color(0xFFFFC72C)
+    val YellowHover = Color(0xFFE5B325)
+    val LightYellow = Color(0xFFFEF3C7)
+
+    val DarkText = Color(0xFF1A1A2E)
+    val GreyText = Color(0xFF6B7280)
+    val LightGrey = Color(0xFF9CA3AF)
+    val Border = Color(0xFFE5E7EB)
+
+    val LightBackground = Color(0xFFF5F7FA)
+    val Surface = Color(0xFFFFFFFF)
+    val CardBackground = Color(0xFFFFFFFF)
+
     val UnreadBlue = Color(0xFFEAF4FF)
+    val SuccessGreen = Color(0xFF10B981)
+    val DangerRed = Color(0xFFD32F2F)
 }
