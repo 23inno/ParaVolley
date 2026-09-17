a98512c7fe7309bfcb1ebb28ad5c86c522d419b1
zookie2004 <24301426@stadio.ac.za>
2026-09-17 14:39:44 +0200

    Add profile update and photo API calls

--- mobile/app/src/main/java/com/paravolley/mobile/network/PlayerApi.kt
+++ mobile/app/src/main/java/com/paravolley/mobile/network/PlayerApi.kt
@@ -1,8 +1,15 @@
 package com.paravolley.mobile.network
 
+import okhttp3.MultipartBody
+import okhttp3.ResponseBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Part
 
 interface PlayerApi {
 
@@ -10,5 +17,26 @@ interface PlayerApi {
     suspend fun getProfile(
         @Header("Authorization")
         authorization: String
+    ): Response<PlayerProfileResponse>
+
+    @PUT("api/player/me")
+    suspend fun updateProfile(
+        @Header("Authorization")
+        authorization: String,
+        @Body request: UpdatePlayerProfileRequest
+    ): Response<PlayerProfileResponse>
+
+    @GET("api/player/me/photo")
+    suspend fun getProfilePhoto(
+        @Header("Authorization")
+        authorization: String
+    ): Response<ResponseBody>
+
+    @Multipart
+    @POST("api/player/me/photo")
+    suspend fun uploadProfilePhoto(
+        @Header("Authorization")
+        authorization: String,
+        @Part photo: MultipartBody.Part
     ): Response<PlayerProfileResponse>
 }
