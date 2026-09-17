1feb3a5a2aea39f88d8ca00ba532e4ed378b7fab
zookie2004 <24301426@stadio.ac.za>
2026-09-17 16:39:33 +0200

    Add notification read endpoints to Android API

--- mobile/app/src/main/java/com/paravolley/mobile/network/AnnouncementsApi.kt
+++ mobile/app/src/main/java/com/paravolley/mobile/network/AnnouncementsApi.kt
@@ -3,6 +3,7 @@ package com.paravolley.mobile.network
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AnnouncementsApi {
@@ -20,4 +21,18 @@ interface AnnouncementsApi {
         @Path("id")
         announcementId: Int
     ): Response<AnnouncementResponse>
+
+    @POST("api/announcements/{id}/read")
+    suspend fun markAnnouncementRead(
+        @Header("Authorization")
+        authorization: String,
+        @Path("id")
+        announcementId: Int
+    ): Response<Unit>
+
+    @POST("api/announcements/read-all")
+    suspend fun markAllAnnouncementsRead(
+        @Header("Authorization")
+        authorization: String
+    ): Response<Unit>
 }
