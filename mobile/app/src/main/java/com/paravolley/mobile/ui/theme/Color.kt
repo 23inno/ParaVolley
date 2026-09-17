3e710314843e7575e30cdafe8c82d83c791411db
zookie2004 <24301426@stadio.ac.za>
2026-09-17 10:11:02 +0200

    Align Android palette with Figma mobile design

--- mobile/app/src/main/java/com/paravolley/mobile/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/paravolley/mobile/ui/theme/Color.kt
@@ -2,17 +2,18 @@ package com.paravolley.mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val ParaGreenPrimary = Color(0xFF0B6E4F)
-val ParaGreenDark = Color(0xFF064E3B)
-val ParaGreenLight = Color(0xFFEAF5F0)
-val ParaGoldSecondary = Color(0xFFFFC72C)
-val ParaGoldHover = Color(0xFFE5B325)
-val ParaBackground = Color(0xFFF5F7FA)
+// Mobile palette aligned with the approved Figma Make player experience.
+val ParaGreenPrimary = Color(0xFF1A5F3F)
+val ParaGreenDark = Color(0xFF154D35)
+val ParaGreenLight = Color(0xFFEAF4EF)
+val ParaGoldSecondary = Color(0xFFFBBF24)
+val ParaGoldHover = Color(0xFFF59E0B)
+val ParaBackground = Color(0xFFF9FAFB)
 val ParaSurface = Color(0xFFFFFFFF)
-val ParaTextDark = Color(0xFF1A1A2E)
+val ParaTextDark = Color(0xFF111827)
 val ParaTextMuted = Color(0xFF6B7280)
 val ParaBorder = Color(0xFFE5E7EB)
-val ParaError = Color(0xFFD32F2F)
-val ParaSuccess = Color(0xFF10B981)
+val ParaError = Color(0xFFDC2626)
+val ParaSuccess = Color(0xFF16A34A)
 val ParaWarningBg = Color(0xFFFEF3C7)
 val ParaWarningText = Color(0xFF92400E)
