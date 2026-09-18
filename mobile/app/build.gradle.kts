0f7fc10bcc6303617cb72f87cbadd8bfe3bd1a76
zookie2004 <24301426@stadio.ac.za>
2026-09-18 17:32:57 +0200

    Bump Android player app beta version

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.paravolley.mobile"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0-beta"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
